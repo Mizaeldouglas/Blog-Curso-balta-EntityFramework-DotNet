a9891099017174bd8537ff91d52e5dcb907f96eb
mizaeldouglas <mizaeldouglas2@gmail.com>
2024-03-31 20:25:39 -0300

    projeto iniciado

--- obj/Debug/net6.0/Blog.AssemblyInfo.cs
+++ obj/Debug/net6.0/Blog.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Blog")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e5aae4c8c8522a4c273c48ee35be5994bc368a9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Blog")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Blog")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
